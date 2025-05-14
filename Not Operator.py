95e4b5e331080f80fcac9186aff1cae48635a21c
rishikisgood <Kaulrishik15@gmail.com>
2025-05-14 17:02:01 +0530

    Add files via upload

---
+++ Not Operator.py
@@ -0,0 +1,22 @@
+A= 10
+B=12
+C=12
+
+print(A !=B)
+print(B !=C)
+
+a="Python"
+b="Coding"
+
+if a != b :
+    print(a, 'and', b, 'are different.')
+
+a=4
+b=5
+
+if(a==1) != (b==5):
+    print("Hello!!!!!!")
+a=int(input("Enter a number"))
+
+if a%2 !=0:
+    print(a, "is not even number.")
